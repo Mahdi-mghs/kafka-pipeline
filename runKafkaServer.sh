d87e5fee23b9002feb86d95941f2f47e1bbe0eab
Moodi <m.maghsud81@gmail.com>
2025-04-15 16:24:03 +0330

    modfied by Gradle

--- runKafkaServer.sh
+++
@@ -1,5 +0,0 @@
-#create topic
-kafka-topics.sh --bootstrap-server 127.0.0.1:9092 --create --topic wikimedia.recentchange --partitions 3 --replication-factor 1
-
-#check LAG status
-kafka-consumer-groups.sh  --bootstrap-server 127.0.0.1:9092 --group consumer-opensearch-demo --describe
